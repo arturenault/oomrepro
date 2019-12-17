0c1ac04c5d296fa1ca853ddc3f1aaa6ec4cc55dc
Artur Upton Renault <aruptonr@microsoft.com>
2019-12-17 12:56:04 -0800

    Add project files.

---
+++ OOMRepro/Controllers/MemoryController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace OOMRepro.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MemoryController : ControllerBase
+    {
+        private readonly ILogger<MemoryController> _logger;
+
+        private readonly Random random;
+
+        public MemoryController(ILogger<MemoryController> logger)
+        {
+            _logger = logger;
+            random = new Random();
+        }
+
+        [HttpGet]
+        public MemoryMetrics Get()
+        {
+            var client = new MemoryMetricsClient();
+            return client.GetMetrics();
+        }
+
+        [HttpPost("{size:int}")]
+        public void Post(int size=1000000)
+        {
+            byte[] data = new byte[8192];
+            var path = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+            using (FileStream stream = System.IO.File.OpenWrite(path))
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    random.NextBytes(data);
+                    stream.Write(data, 0, data.Length);
+                }
+            }
+        }
+    }
+}
---
+++ OOMRepro/MemoryMetricsClient.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace OOMRepro
+{
+    public class MemoryMetrics
+    {
+        public double Total { get; set; }
+        public double Used { get; set; }
+        public double Free { get; set; }
+    }
+
+    public class MemoryMetricsClient
+    {
+        public MemoryMetrics GetMetrics()
+        {
+            if (IsUnix())
+            {
+                return GetUnixMetrics();
+            }
+
+            return GetWindowsMetrics();
+        }
+
+        private bool IsUnix()
+        {
+            var isUnix = RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ||
+                         RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+
+            return isUnix;
+        }
+
+        private MemoryMetrics GetWindowsMetrics()
+        {
+            var output = "";
+
+            var info = new ProcessStartInfo();
+            info.FileName = "wmic";
+            info.Arguments = "OS get FreePhysicalMemory,TotalVisibleMemorySize /Value";
+            info.RedirectStandardOutput = true;
+
+            using (var process = Process.Start(info))
+            {
+                output = process.StandardOutput.ReadToEnd();
+            }
+
+            var lines = output.Trim().Split("\n");
+            var freeMemoryParts = lines[0].Split("=", StringSplitOptions.RemoveEmptyEntries);
+            var totalMemoryParts = lines[1].Split("=", StringSplitOptions.RemoveEmptyEntries);
+
+            var metrics = new MemoryMetrics();
+            metrics.Total = Math.Round(double.Parse(totalMemoryParts[1]) / 1024, 0);
+            metrics.Free = Math.Round(double.Parse(freeMemoryParts[1]) / 1024, 0);
+            metrics.Used = metrics.Total - metrics.Free;
+
+            return metrics;
+        }
+
+        private MemoryMetrics GetUnixMetrics()
+        {
+            var output = "";
+
+            var info = new ProcessStartInfo("free -b");
+            info.FileName = "/bin/bash";
+            info.Arguments = "-c \"free -b\"";
+            info.RedirectStandardOutput = true;
+            info.RedirectStandardError = true;
+
+            using (var process = Process.Start(info))
+            {
+                output = process.StandardOutput.ReadToEnd();
+                Console.WriteLine(output);
+            }
+
+            var lines = output.Split("\n");
+            var memory = lines[1].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            var metrics = new MemoryMetrics();
+            metrics.Total = double.Parse(memory[1]);
+            metrics.Used = double.Parse(memory[2]);
+            metrics.Free = double.Parse(memory[3]);
+
+            return metrics;
+        }
+    }
+}
